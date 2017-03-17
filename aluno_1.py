ae16c48d9672b14c9560037f9bcdb57daf3861c7
Ari Halpern <arihalpern@Aris-MacBook-Air.local>
2017-03-17 11:17:35 -0300

    oi

---
+++ aluno_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 10:30:35 2017
+
+@author: arihalpern
+"""
+def ler_numeros (n):
+    for i in range(12):
+        i = int(input(n))
+    return i
+ler_numeros("diga um numero")
